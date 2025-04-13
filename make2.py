f333eea909ca0e61af6cfac8a6d8f1ec6929e634
Ethan Brown <browneb22@juniata.edu>
2025-04-13 14:20:31 -0400

    Adding the second python file

---
+++ make2.py
@@ -0,0 +1,9 @@
+# Ethan Brown
+# Second Python Program for Makefile MiniProject
+# Scientific Computing
+# April 13, 2025
+
+if __name__ == '__main__':
+        outFile = open("make2.txt","w")
+        for i in range(20):
+                outFile.write(str(i) + "\n")
